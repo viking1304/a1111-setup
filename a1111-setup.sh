0d99f6c8eecf3f2146cfb66973378de60cd5470e
viking1304 <viking1304@gmail.com>
2023-12-16 16:08:51 +0100

    Version 0.2

--- a1111-setup.sh
+++ a1111-setup.sh
@@ -12,9 +12,9 @@
 # Bugs: ---
 # Notes: ---
 # Author: Aleksandar Milanovic (viking1304)
-# Version: 0.0.1
+# Version: 0.0.2
 # Created: 2023/12/12 19:30:51
-# Last modified: 2022/12/12 20:55:07
+# Last modified: 2022/16/12 16:00:07
 
 # Copyright (c) 2023 Aleksandar Milanovic
 # https://github.com/viking1304/
@@ -37,6 +37,11 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+# install stable version of PyTorch and only fix errors by default
+update_brew=false
+torch="stable"
+fix="errors"
+
 # Install Homebrew
 install_homebrew() {
   echo "Checking for Homebrew..."
@@ -44,22 +49,24 @@ install_homebrew() {
     echo "Installing Homebrew..."
     echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   else
-    brew update
-    brew upgrade
+    if [[ $update_brew == true ]]; then
+      brew update
+      brew upgrade
+    fi
   fi
 }
 
 detect_cpu() {
-  echo "Detecting processor..."
+  echo "\nDetecting processor..."
   if [[ "$(sysctl -n machdep.cpu.brand_string)" =~ ^.*"Apple".*$ ]]; then
-    echo "ARM processor detected"
+    echo "ARM processor detected\n"
 
     # temporary add Brew to path on ARM
     eval "$(/opt/homebrew/bin/brew shellenv)"
   fi
 
   if [[ "$(sysctl -n machdep.cpu.brand_string)" =~ ^.*"Intel".*$ ]]; then
-      echo "Intel processor detected"
+      echo "Intel processor detected\n"
   fi
 }
 
@@ -68,13 +75,22 @@ install_a1111() {
   cd
   # check if destination folder exists
   if [[ ! -d "stable-diffusion-webui" ]]; then
-    echo "New installation. Cloning A1111..."
+    echo "\nNew installation. Cloning A1111..."
     # clone automatic1111
     git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui
     cd stable-diffusion-webui
   else
-    echo "Existing installation detected..."
+    echo "\nExisting installation detected..."
     cd stable-diffusion-webui
+    # force A1111 upgrade
+    if [[ -d ".git" ]]; then
+      echo "Forcing A1111 upgrade..."
+      git reset --hard origin/master
+      git pull
+    else
+      echo "\nCurrent version is not installed using git!\nPlease rename or remove folder ${HOME}/stable-diffusion-webui and try again"
+      exit 1
+    fi
     # purge pip cache
     if [[ -f "venv/bin/pip" ]]; then  
       echo "Purging pip cache..."
@@ -84,11 +100,6 @@ install_a1111() {
     if [[ -d "venv" ]]; then  
       echo "Removing venv..."
       rm -rf venv
-    fi
-    # ensure that correct files are used
-    if [[ -d ".git" ]]; then
-      echo "Forcing use of latest file versions..."
-      git reset --hard origin/master
     fi
   fi
 }
@@ -103,22 +114,61 @@ brew_install() {
 }
 
 apply_fixes() {
-  echo "Instruct A1111 to use development version of torch..."
-  sed -i '' 's/# install command for torch/# install command for torch\nexport TORCH_COMMAND="pip install --pre torch torchvision torchaudio --index-url https:\/\/download.pytorch.org\/whl\/nightly\/cpu"/' webui-user.sh
-  echo "Add recommended command line parameters..."
-  sed -i '' 's/#export COMMANDLINE_ARGS=""/#export COMMANDLINE_ARGS=""\nexport COMMANDLINE_ARGS="--skip-torch-cuda-test --opt-sub-quad-attention --upcast-sampling --no-half-vae --medvram-sdxl"/' webui-user.sh
-  echo "Fix import of torchvision 0.17+..."
-  sed -i '' 's/    sys.modules\["xformers"\] = None/    sys.modules\["xformers"\] = None\n\n# Hack to fix a changed import in torchvision 0.17+, which otherwise breaks\n# basicsr; see https:\/\/github.com\/AUTOMATIC1111\/stable-diffusion-webui\/issues\/13985\ntry:\n    import torchvision.transforms.functional_tensor  # noqa: F401\nexcept ImportError:\n    try:\n        import torchvision.transforms.functional as functional\n        sys.modules\["torchvision.transforms.functional_tensor"\] = functional\n    except ImportError:\n        pass  # shrug.../' modules/import_hook.py
-  echo "Fix cannot convert a MPS Tensor to float64 dtype error..."
-  sed -i '' "/^                    dtype = sd_param.dtype if sd_param is not None else param.dtype/,/^ *[^:]*:/s/module._parameters\[name\] = torch.nn.parameter.Parameter/if dtype == torch.float64 and device.type == 'mps':\n                      dtype = torch.float32\n                    module._parameters\[name\] = torch.nn.parameter.Parameter/" modules/sd_disable_initialization.py
+  if [[ "$torch" == "develop" ]]; then
+    echo "\nInstruct A1111 to use development version of torch..."
+    sed -i '' 's/#export TORCH_COMMAND="pip install torch==1.12.1+cu113 --extra-index-url https:\/\/download.pytorch.org\/whl\/cu113"/#export TORCH_COMMAND="pip install torch==1.12.1+cu113 --extra-index-url https:\/\/download.pytorch.org\/whl\/cu113"\nexport TORCH_COMMAND="pip install --pre torch torchvision torchaudio --index-url https:\/\/download.pytorch.org\/whl\/nightly\/cpu"/' webui-user.sh
+  else
+    echo "\nInstruct A1111 to use latest stable version of torch..."
+    sed -i '' 's/#export TORCH_COMMAND="pip install torch==1.12.1+cu113 --extra-index-url https:\/\/download.pytorch.org\/whl\/cu113"/#export TORCH_COMMAND="pip install torch==1.12.1+cu113 --extra-index-url https:\/\/download.pytorch.org\/whl\/cu113"\nexport TORCH_COMMAND="pip install torch torchvision torchaudio"/' webui-user.sh
+  fi
+  if [[ "$fix" == "all" ]]; then
+    echo "Add recommended command line parameters..."
+    sed -i '' 's/#export COMMANDLINE_ARGS=""/#export COMMANDLINE_ARGS=""\nexport COMMANDLINE_ARGS="--skip-torch-cuda-test --opt-sub-quad-attention --upcast-sampling --no-half-vae --medvram-sdxl"/' webui-user.sh
+  fi
+  if [[ "$fix" == "all" || "$fix" == "errors" ]]; then
+    echo "Fix cannot convert a MPS Tensor to float64 dtype error..."
+    sed -i '' "/^                    dtype = sd_param.dtype if sd_param is not None else param.dtype/,/^ *[^:]*:/s/module._parameters\[name\] = torch.nn.parameter.Parameter/if dtype == torch.float64 and device.type == 'mps':\n                      dtype = torch.float32\n                    module._parameters\[name\] = torch.nn.parameter.Parameter/" modules/sd_disable_initialization.py
+  fi
+}
+
+parase_parameters() {
+  usage() { echo "$0 usage:" && grep "[[:space:]].)\ #" $0 | sed 's/#//' | sed -r 's/([a-z])\) /[-\1/'; exit 0; }
+  #(( $# == 0 )) && usage
+  while getopts ":hbt:f:" arg; do
+    case $arg in
+      t) # stable|develop] stable or develop version of PyTorch
+        torch=${OPTARG}
+        if [[ "$torch" != "stable"  && "$torch" != "develop" ]]; then
+          echo "parameter -t must either be stable or develop"
+          exit 1
+        fi
+        ;;
+      f) # all|errors|none] apply all fixes, only fixes for errors or none
+        fix=${OPTARG}
+        if [[ "$fix" != "all"  && "$fix" != "errors" && "$fix" != "none" ]]; then
+          echo "parameter -f must be all, errors or none"
+          exit 1
+        fi
+        ;;
+      b) #] update Homebrew
+        update_brew=true
+        ;;
+      h) #] display help
+        usage
+        exit 0
+        ;;
+    esac
+  done
 }
 
 main() {
   # Exit script if not run on macOS
   if [[ "$(uname -s)" != "Darwin" ]]; then
-    err "" "This script can only be used on macOS!" "${ERR_COLOR}"
+    echo "This script can only be used on macOS!"
     exit 1
   fi  
+
+  parase_parameters "$@"
 
   # Ask for user password upfront (with custom prompt)
   sudo -v -p "Please enter password for user '%p': "
@@ -133,6 +183,7 @@ main() {
   detect_cpu
 
   # install required packages
+  echo "Checking for required packages..."
   brew_install cmake
   brew_install protobuf 
   brew_install rust 
